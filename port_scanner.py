f13761d0606d149105a9259af1e208eb2cab6c3d
DRHood <darryl.r.hood@gmail.com>
2024-04-21 12:16:52 -0400

    def port_scanner ccreated

--- port_scanner.py
+++ port_scanner.py
@@ -6,3 +6,9 @@ port = 80
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Set a timeout
 s.settimeout(5)
+
+def port_scanner(port):
+    if s.connect_ex((target, port)):
+        print("The port is closed")
+    else:
+        print("The port is open")
