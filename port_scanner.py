1365197fe28b9c8912102ba13e7c46b1824a0db6
DRHood <darryl.r.hood@gmail.com>
2024-04-27 10:29:50 -0400

    try syntax error corrected

--- port_scanner.py
+++ port_scanner.py
@@ -1,23 +1,20 @@
 import socket
 import threading
 
-#User input for target Ip address
+# User input for target Ip address
 target = input("Enter target IP address to scan: ")
 
 def port_scanner(port):
-    Try:
+    try:   
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(5)
-        con = s.connect((target, port))
+        con = s.connect((target,port))
         print(f'The port {port} is open')
         con.close()
     except:
         pass
 
-
 # Scanning ports using threading for speed
-for port in range(0, 1025):
+for port in range(start_port, end_port+1):
     thread = threading.Thread(target = port_scanner, args = (port, ))
     thread.start()
-
-port_scanner(port)
