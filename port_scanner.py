d42228886203d5d1f6cdd90f3a537a5a879a512b
DRHood <darryl.r.hood@gmail.com>
2024-04-27 10:45:02 -0400

    set specific port range for scanner

--- port_scanner.py
+++ port_scanner.py
@@ -8,13 +8,13 @@ def port_scanner(port):
     try:   
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(5)
-        con = s.connect((target,port))
+        con = s.connect((target, port))
         print(f'The port {port} is open')
         con.close()
     except:
         pass
 
 # Scanning ports using threading for speed
-for port in range(start_port, end_port+1):
+for port in range(0, 1025):
     thread = threading.Thread(target = port_scanner, args = (port, ))
     thread.start()
